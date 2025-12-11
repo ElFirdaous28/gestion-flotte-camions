5ef6ec9bab567f19fa7340c885899d5ac11f603c
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-12-11 09:33:18 +0100

    GFC-20 FuelLog CRUD

---
+++ backend/routes/fuelEntry.routes.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import {
+    createFuelEntry,
+    getFuelEntries,
+    getFuelEntry,
+    updateFuelEntry,
+    deleteFuelEntry,
+    getFuelEntriesByTrip
+} from '../controllers/fuelEntry.controller.js';
+
+import isAuthenticated from '../middlewares/auth.middleware.js';
+import authorizedRoles from '../middlewares/authorize.middleware.js';
+import validate from '../middlewares/validate.middleware.js';
+import validateObjectId from '../middlewares/objectId.middelware.js';
+import { FuelEntrySchema } from '../validation/fuelEntry.shcema.js';
+
+const router = express.Router();
+router.use(isAuthenticated);
+
+router.get('/', authorizedRoles('admin'), getFuelEntries);
+router.get('/:id', validateObjectId(), getFuelEntry);
+router.post('/', authorizedRoles('admin'), validate(FuelEntrySchema), createFuelEntry);
+router.put('/:id', validateObjectId(), authorizedRoles('admin'), validate(FuelEntrySchema), updateFuelEntry);
+router.delete('/:id', validateObjectId(), authorizedRoles('admin'), deleteFuelEntry);
+router.get('/trip/:tripId', validateObjectId('tripId'), getFuelEntriesByTrip);
+
+export default router;
