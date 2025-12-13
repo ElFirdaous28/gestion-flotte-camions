d21f6517993d063c7891b4ef2c90cf6e5c4f7ced
ElFirdaous28 <elmokhtarifirdaous@gmqil.com>
2025-12-13 20:44:04 +0100

    GFC-17 login page

--- frontend/src/routes/AdminRoutes.jsx
+++ frontend/src/routes/AdminRoutes.jsx
@@ -1,6 +1,6 @@
 import { Route } from 'react-router-dom';
 import { lazy } from 'react';
-
+ 
 const Dashboard = lazy(() => import('../pages/admin/Dashboard'));
 
 export default function AdminRoutes() {
